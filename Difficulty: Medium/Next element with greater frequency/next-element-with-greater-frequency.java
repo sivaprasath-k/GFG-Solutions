913cc01c62b048e54a2bb100851ed00d440b9852
SIVAPRASATH K <sivaprasathsivaprasath123@gmail.com>
2026-01-18 20:00:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
@@ -1,29 +1,50 @@
 class Solution {
-    public ArrayList<Integer> findGreater(int[] arr) {
+    public ArrayList<Integer> nextFreqGreater(int[] arr) {
         // code here
+        int len=arr.length,flag=0;
         HashMap<Integer,Integer> map=new HashMap<>();
+        for(int val:arr){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
         Stack<Integer> st=new Stack<>();
-        ArrayList<Integer> a=new ArrayList<>();
-        for(int i:arr){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(int i=arr.length-1;i>=0;i--){
-            while(!st.isEmpty()){
-                int x=st.peek();
-                if(map.get(x)>map.get(arr[i])){
-                    a.add(x);
-                    break;
-                }
-                else{
-                    st.pop();
-                }
+        ArrayList<Integer> res=new ArrayList<>();
+        int i=len-1;
+        while(i>=0){
+            int fq=map.get(arr[i]);
+            while(!st.isEmpty() && fq>=map.get(st.peek())){
+                st.pop();
             }
-            if(st.isEmpty()){
-                a.add(-1);
-            }
-            st.push(arr[i]);
+            if(st.isEmpty()) res.add(-1);
+            else res.add(st.peek());
+            st.add(arr[i]);
+            i--;
         }
-        Collections.reverse(a);
-        return a;
+        Collections.reverse(res);
+        return res;
     }
 }
+
+// class Solution {
+//     public ArrayList<Integer> nextFreqGreater(int[] arr) {
+//         // code here
+//         int len=arr.length,flag=0;
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int val:arr){
+//             map.put(val,map.getOrDefault(val,0)+1);
+//         }
+//         ArrayList<Integer> res=new ArrayList<>();
+//         for(int i=0;i<len;i++){
+//             int fre=map.get(arr[i]);
+//             for(int j=i+1;j<len;j++){
+//                 if(map.get(arr[j])>fre){
+//                     res.add(arr[j]);
+//                     flag=1;
+//                     break;
+//                 }
+//             }
+//             if(flag==0) res.add(-1);
+//             flag=0;
+//         }
+//         return res;
+//     }
+// }
